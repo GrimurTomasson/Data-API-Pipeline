3889717b870c70af1cb41c5239c3874236479882
Grímur Tómas Tómasson <Grimur.Tomas.Tomasson@reykjavik.is>
2022-12-16 13:39:22 +0000

    Latest made single step compliant

--- processes/Latest.py
+++ processes/Latest.py
@@ -1,9 +1,15 @@
+import sys
+import argparse
+
 from Shared.Decorators import output_headers, execution_time
 from Shared.Config import Config
 from Shared.Utils import Utils
 from Shared.Logger import Logger
 
 class Latest:
+    _argParser = argparse.ArgumentParser (prog='Latest.py', description='Creates new relations and tests them.')
+    _argParser.add_argument ('operation', choices=['build', 'test'])
+
     def __init__ (self) -> None:
         return
         
@@ -24,3 +30,13 @@ class Latest:
         Logger.debug (f"\tOutput for dbt test results: {Config.dbtTestOutputFileInfo.qualified_name}")
         Utils.write_file (output.stdout, Config.dbtTestOutputFileInfo.qualified_name)
         return
+
+def main (args):
+    options = Latest._argParser.parse_args (args)
+    if options.operation == 'build':
+        return Latest ().refresh ()
+    elif options.operation == 'test':
+        return Latest ().run_tests ()
+
+if __name__ == '__main__':
+    main (sys.argv[1:]) # Getting rid of the filename
