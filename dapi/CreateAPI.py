2d086c0ed403209a88406c097bc177a052edea58
Grímur Tómas Tómasson <grimur@gthugbunadarradgjof.com>
2023-03-03 16:23:01 +0000

    Updated docs and creation script, needs further work!

--- dapi/CreateAPI.py
+++ dapi/CreateAPI.py
@@ -1,88 +1,77 @@
 import os
 import shutil
-import platform
-import yaml
 
-configName = 'api_config.yml'
-pipelineProcesses = 'Data-API-Pipeline/processes'
-implementationTemplates = 'implementation_templates'
+from .Shared import Utils
 
-templates = ['api_documentation_template.md', 'GenerateAPI.py']
+class CreateApi:
 
-pathDelimiter = '/'
-if platform.system() == 'Windows':
-    pathDelimiter = '\\'
+    def __init__ (self) -> None:
+        self._packageName = 'dapi'
+        self._configName = 'api_config.yml'
+        self._implementationTemplates = os.path.join(self._packageName, 'ImplementationTemplates')
 
-workingDir = os.getcwd ()
-superPath, workingDirName = os.path.split (workingDir)
+        self._templates = ['api_documentation_template.md', 'api_config.yml', 'data-api-pipeline.env']
 
-def directory_scan (path):
-    for entry in os.scandir(path):
-        try:
-            if entry.is_dir (follow_symlinks=False):
-                yield from directory_scan (entry.path)
-                yield entry
-        except OSError as error:
-            pass #print (f"directory_scan error: {error}")
+        self._workingDir = os.getcwd ()
+        self._superPath, self._workingDirName = os.path.split (self._workingDir)
 
-def find_directory(path, endsWith):
-    """Find a directory whose path contains a pattern."""
-    endsWith = endsWith.replace ('\\', pathDelimiter)
-    endsWith = endsWith.replace ('/', pathDelimiter)
+        return
 
-    for entry in directory_scan (path):
-        if str (entry.path).endswith (endsWith):
-            return entry.path
-    return ""
+    def __find_package (self) -> str:
+        operation = ['pip3', 'show', self._packageName]
+        packageInfo = Utils.run_operation (self._workingDir, self._workingDir, operation, True)
+        print (packageInfo)
+        return 'smu'
 
-def copy_templates () -> None:
-    print ("Copying template files")
-    for template in templates:
-        qualifiedFilename = os.path.join (templateDir, template)
-        qualifiedTarget = os.path.join (workingDir, template)
-        shutil.copy2 (qualifiedFilename, qualifiedTarget)
-        print (f"\t{template} copied to {workingDir}")
-    return
+    def __copy_templates (self) -> None:
+        print ("Copying template files")
+        for template in self._templates:
+            qualifiedFilename = os.path.join (self._templateDir, template)
+            qualifiedTarget = os.path.join (self._workingDir, template)
 
-def process_config () -> None:
-    print (f"\nEditing {configName}")
-    qualifiedConfig = os.path.join (os.path.join (pipelineDirectory, implementationTemplates), configName)
-    relativePath = os.path.relpath (pipelineDirectory, os.getcwd ())
+            shutil.copy2 (qualifiedFilename, qualifiedTarget)
+            print (f"\t{template} copied to {self._workingDir}")
+        return
 
-    with open (qualifiedConfig, mode="r", encoding="utf-8") as f:
-        config = f.read ()
+    def __process_config (self) -> None:
+        print (f"\nEditing {self._configName}")
+        qualifiedConfig = os.path.join (os.path.join (self._pipelineDirectory, self._implementationTemplates), self._configName)
 
-    # Við breytum þessu með strengjaleikfimi frekar en yml til þess að tapa ekki athugasemdum í config!
-    config = config.replace ('?/Data-API-Pipeline/processes', relativePath)
-    print (f"\tRelative path to pipeline repo: {relativePath}")
+        with open (qualifiedConfig, mode="r", encoding="utf-8") as f:
+            config = f.read ()
 
-    config = config.replace ('?SOME-API', workingDirName)
-    print (f"\tDatabase name: {workingDirName}")
+        # Við breytum þessu með strengjaleikfimi frekar en yml til þess að tapa ekki athugasemdum í config!
+        config = config.replace ('?SOME-API', self._workingDirName)
+        print (f"\tDatabase name: {self._workingDirName}")
 
-    targetConfig = os.path.join (os.getcwd (), configName)
-    with open (targetConfig, mode="w", encoding="utf-8") as f:
-        f.write (config)
-    print ("\nUpdated config written")
-    return
+        targetConfig = os.path.join (os.getcwd (), self._configName)
+        with open (targetConfig, mode="w", encoding="utf-8") as f:
+            f.write (config)
+        print ("\nUpdated config written")
+        return
         
-rootDirectory = os.path.abspath (os.sep)
 
-print (f"\nWorking directory: {workingDir}")
-print (f"Working directory-, API-, Database name -> {workingDirName}")
+    def generate (self) -> None:
+        print (f"\nWorking directory: {self._workingDir}")
+        print (f"Working directory-, API-, Database name -> {self._workingDirName}")
 
-print ("\nLooking for the Data-API-Pipeline repo")
-pipelineLocation = find_directory (rootDirectory, pipelineProcesses)
-if len (pipelineLocation) == 0:
-    print ("We cannot find the location of the Data-API-Pipeline repo!")
-    exit(1)
+        print (f"\nLooking for the {self._packageName} package location")
+        pipelineLocation = self.__find_package ()
+        if len (pipelineLocation) == 0:
+            print (f"We cannot find the location of the {self._packageName} package!")
+            exit(1)
 
-pipelineDirectory = os.path.abspath (os.path.join (pipelineLocation, '..'))
-print (f"Data-API-Pipeline found at: {pipelineDirectory}\n")
+        pipelineDirectory = os.path.abspath (os.path.join (pipelineLocation, '..'))
+        print (f"{self._packageName} found at: {pipelineDirectory}\n")
 
-templateDir = os.path.abspath (os.path.join (pipelineDirectory, implementationTemplates))
-print (f"Template directory: {templateDir}\n")
+        templateDir = os.path.abspath (os.path.join (pipelineDirectory, self._implementationTemplates))
+        print (f"Template directory: {templateDir}\n")
 
-copy_templates ()
-process_config ()
+        self.__copy_templates ()
+        self.__process_config ()
 
-print ("\nAll done! Remember to edit the config!\n")
+        print ("\nAll done! Remember to edit the config!\n")
+        return
+
+if __name__ == '__main__':
+    CreateApi ().generate ()
