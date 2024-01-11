737e14a6678f8dc1a6cdcb4c0613937802287e80
Grímur Tómas Tómasson <Grimur.Tomas.Tomasson@reykjavik.is>
2024-01-11 12:06:01 +0000

    dbt patch virkni bætt við, til að fá documentation fyrir private grunna rétta

--- dapi/package_cli.py
+++ dapi/package_cli.py
@@ -16,6 +16,7 @@ from .DataHealthReport import DataHealthReport
 from .MetadataCatalog import MetadataCatalog
 from .DefinitionHealthReport import DefinitionHealthReport
 from .Documentation import Documentation
+from .PatchDbt import PatchDbt
 
 from .Shared.Audit import Audit
 from .Shared.Logger import Logger
@@ -28,22 +29,23 @@ argParser = argparse.ArgumentParser (
     formatter_class=argparse.RawTextHelpFormatter)
 
 argParser.add_argument ('operation', 
-                        choices=['build', 'build-data', 'cleanup', 'dependencies', 'refresh', 'test', 'history', 'data-health-report', 'enrich-metadata', 'definition-health-report', 'documentation'], 
+                        choices=['build', 'build-data', 'cleanup', 'dependencies', 'refresh', 'test', 'history', 'data-health-report', 'enrich-metadata', 'definition-health-report', 'documentation', 'patch-dbt'], 
                         help='''
 Aside from build & cleanup, the following steps require that previous steps, from cleanup forward have been run. 
 The steps generate data files, so a single step can be run multiple times as long as those files exist.
 The only parameter for operations is en environment file, which is optional.
                         
-0. build                        Runs the following steps.
-1. cleanup                      Removes run files from previous runs.
-2. dependencies                 Makes sure we have dbt plugins installed and a connection to the DB.
-3. refresh                      Creates relations (tables & views) from the dbt models.
-4. history                      Creates a history snapshot of the results from refresh.
-5. test                         Runs dbt tests.
-6. data-health-report           Generates a markdown data health report and publishes it.
-7. enrich-metadata              Enriches metadata using a concept glossary and target database metadata.
-8. definition-health-report     Generates a markdown API definition health report, using the enriched metadata and publishes it.
-9. documentation                Generates markdown end-user documentation and publishes it.
+0.  build                        Runs the following steps.
+1.  cleanup                      Removes run files from previous runs.
+2.  dependencies                 Makes sure we have dbt plugins installed and a connection to the DB.
+3.  refresh                      Creates relations (tables & views) from the dbt models.
+4.  history                      Creates a history snapshot of the results from refresh.
+5.  test                         Runs dbt tests.
+6.  data-health-report           Generates a markdown data health report and publishes it.
+7.  enrich-metadata              Enriches metadata using a concept glossary and target database metadata.
+8.  definition-health-report     Generates a markdown API definition health report, using the enriched metadata and publishes it.
+9.  documentation                Generates markdown end-user documentation and publishes it.
+10. patch-dbt                    Patch dbt bugs, empty if the current version pair has no problems.
                                 ''')
 argParser.add_argument ('-e', '--environment', required=False, help='Select an environment file to load.')
 argParser.add_argument('-d', '--dbt_run_parameters', required=False, type=str, help='Add any dbt parameters for the run command.')
@@ -91,5 +93,7 @@ def main ():
         DefinitionHealthReport ().generate () # Skrifar skrá: 6
     elif options.operation == 'documentation':
         Documentation ().generate () # Skrifar skrá: 7
+    elif options.operation == 'patch-dbt':
+        PatchDbt ().patch()
     else:
         argParser.print_help()
