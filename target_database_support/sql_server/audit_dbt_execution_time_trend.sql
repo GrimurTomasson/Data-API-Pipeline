6c21eb904b7bf8e86800062f96f91ed0ff5bd69c
Grímur Tómasson <118596444+GrimurTomasson@users.noreply.github.com>
2024-04-24 14:48:18 +0000

    Uppfærslur út af dbt 1.7 væðingu

---
+++ target_database_support/sql_server/audit_dbt_execution_time_trend.sql
@@ -0,0 +1,18 @@
+WITH gogn AS (
+	SELECT 
+		operation
+		,execution_time_in_seconds
+		,CAST (start_time AS date) AS dags
+	FROM 
+		audit.dbt_invocation
+	WHERE
+		status = 'success'
+)
+SELECT DISTINCT
+	operation
+	,ROUND (AVG (execution_time_in_seconds) OVER (PARTITION BY operation, dags), 0) AS seconds
+	,dags
+FROM
+	gogn
+ORDER BY 
+	operation, dags
